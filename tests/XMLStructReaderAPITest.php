fd00c1d1c3e7b18b3337efdced73c16083e6521f
Taihao Zhang <jason@zth.me>
2012-05-25 03:55:22 +1000

    Refactored tests for option methods to specific tests.

--- tests/XMLStructReaderAPITest.php
+++ tests/XMLStructReaderAPITest.php
@@ -21,13 +21,41 @@ class XMLStructReaderAPITest extends PHPUnit_Framework_TestCase {
    * @depends testObject
    * @dataProvider delegateProvider
    */
-  public function testOptions($delegate) {
-    $reader = new TestXMLStructReader($delegate, array('test1' => 'custom'));
-    $this->assertEquals('default', $reader->getOption('test2'), 'A default option can be retrieved.');
-    $this->assertEquals('custom', $reader->getOption('test1'), 'A custom option can be retrieved.');
+  public function testGetOption($delegate) {
+    $reader = new TestXMLStructReader($delegate);
+    $this->assertEquals('default', $reader->getOption('test1'), 'A default option can be retrieved.');
+    $this->assertNull($reader->getOption('invalid'), 'An invalid option is NULL.');
+  }
+
+  /**
+   * @depends testGetOption
+   * @dataProvider delegateProvider
+   */
+  public function testSetOption($delegate) {
+    $reader = new TestXMLStructReader($delegate);
+    $reader->setOption('test1', 'custom1');
+    $this->assertEquals('custom1', $reader->getOption('test1'), 'A custom option can be set.');
+  }
+
+  /**
+   * @depends testSetOption
+   * @dataProvider delegateProvider
+   */
+  public function testSetOptions($delegate) {
+    $reader = new TestXMLStructReader($delegate, array('test1' => 'custom1'));
+    $this->assertEquals('custom1', $reader->getOption('test1'), 'Custom options can be set via the constructor.');
+    $reader->setOptions(array('test2' => 'custom2'));
+    $this->assertEquals('custom2', $reader->getOption('test2'), 'Custom options can be set via the method.');
+  }
+
+  /**
+   * @depends testSetOptions
+   * @dataProvider delegateProvider
+   */
+  public function testResetOptions($delegate) {
+    $reader = new TestXMLStructReader($delegate, array('test1' => 'custom1'));
     $reader->resetOptions();
     $this->assertEquals('default', $reader->getOption('test1'), 'A custom option can be reset.');
-    $this->assertNull($reader->getOption('invalid'), 'An invalid option is NULL.');
   }
 
   /**
